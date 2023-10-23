a4ab7f4571bc49ea18276a84a7dc6b5387ef0e11
Elkin Aguas <elkinaguas@gmail.com>
2023-10-23 19:57:43 +0200

    Color changes

--- clir/tools/functions.py
+++ clir/tools/functions.py
@@ -40,12 +40,12 @@ def choose_command(commands: dict = {}):
 
 
 def command_table(commands: dict = {}):
-    table = Table(show_lines=True, box=box.ROUNDED, style="#7D7C7C")
+    table = Table(show_lines=True, box=box.ROUNDED, style="grey46")
 
-    table.add_column("ID 📇", style="cyan", no_wrap=True)
-    table.add_column("Command 💻", style="cyan", no_wrap=True)
+    table.add_column("ID 📇", style="white bold", no_wrap=True)
+    table.add_column("Command 💻", style="green", no_wrap=True)
     table.add_column("Description 📕", style="magenta")
-    table.add_column("Tag 🏷️", style="green")
+    table.add_column("Tag 🏷️", style="cyan")
     
     for indx, command in enumerate(commands):
         desc_len = 50
