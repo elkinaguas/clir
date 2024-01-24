3f0b14aba0d2b232e78778b83102a873c8fdc78d
Elkin Aguas <elkinaguas@gmail.com>
2024-01-24 21:15:06 +0000

    Change call to clir modules

--- clir/cli.py
+++ clir/cli.py
@@ -2,8 +2,8 @@ import rich_click as click
 import os
 import subprocess
 from rich.prompt import Prompt
-from clir.utils.objects import Command
-from clir.utils.objects import CommandTable
+from clir.modules import Command
+from clir.modules import CommandTable
 
 @click.group()
 def cli():
