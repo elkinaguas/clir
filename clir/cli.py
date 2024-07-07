31305fbdd1ac802443516b1cdd6d0d33048eb54d
Elkin Aguas <elkinaguas@gmail.com>
2024-07-07 09:30:41 +0000

    Configure initial configuration as functions

--- clir/cli.py
+++ clir/cli.py
@@ -4,53 +4,49 @@ import subprocess
 from rich.prompt import Prompt
 from clir.command import Command
 from clir.command import CommandTable
+from clir.utils.db import create_database
+from clir.utils.config import check_config, init_config
 
 @click.group()
 def cli():
     pass
 
-def check_config():
-    dir_path = os.path.join(os.path.expanduser('~'), '.clir')
-    file_path = os.path.join(dir_path, 'commands.json')
-
-    return os.path.exists(file_path)
+#def check_config():
+#    dir_path = os.path.join(os.path.expanduser('~'), '.clir')
+#    file_path = os.path.join(dir_path, 'commands.json')
+#    return os.path.exists(file_path)
 
 #--------------------------------------- CLI commands  -------------------------------------------------------
 
-@cli.command(help="Clir initial configuration 🛠️")
-def init():
-    dir_path = os.path.join(os.path.expanduser('~'), '.clir')
-    os.makedirs(dir_path, exist_ok=True)
+#@cli.command(help="Clir initial configuration 🛠️")
+#def init():
+#    dir_path = os.path.join(os.path.expanduser('~'), '.clir')
+#    os.makedirs(dir_path, exist_ok=True)
     
     # Define the file path and name
-    files = ['commands.json', 'credentials.json']
+#    files = ['commands.json', 'credentials.json']
 
     # Check if the file already exists
-    for file in files:
-        file_path = os.path.join(dir_path, file)
-        if not os.path.exists(file_path):
-            # Create the file
-            with open(file_path, 'w') as file_object:
-                file_object.write('{}')
+#    for file in files:
+#        file_path = os.path.join(dir_path, file)
+#        if not os.path.exists(file_path):
+#            # Create the file
+#            with open(file_path, 'w') as file_object:
+#                file_object.write('{}')
 
-            print(f'File "{file_path}" created successfully.')
-        else:
-            print(f'A clir environment already exists in "{dir_path}".')
+#            print(f'File "{file_path}" created successfully.')
+#        else:
+#            print(f'A clir environment already exists in "{dir_path}".')
+    
+#    create_database()
+
 
 @cli.command(help="Save new command 💾")
 @click.option('-c', '--command', help="Command to be saved", prompt=True)
 @click.option('-d', '--description', help="Description of the command", prompt=True)
 @click.option('-t', '--tag', help="Tag to be associated with the command", prompt=True)
 def new(command, description, tag):
-    if not check_config():
-        print("The initial configuration is not set. Executing 'clir init'...")
-        subprocess.run(["clir", "init"])
-
-    # Check again after executing 'clir init'
-    if not check_config():
-        print("Could not set the initial configuration. Unable to add the new command.")
-        return
-    
+    init_config()
     new_command = Command(command = command, description = description, tag = tag)
     new_command.save_command()
 
@@ -58,6 +54,7 @@ def new(command, description, tag):
 @click.option('-t', '--tag', help="Search by tag")
 @click.option('-g', '--grep', help="Search by grep")
 def rm(tag: str = "", grep: str = ""):
+    init_config()
     table = CommandTable(tag=tag, grep=grep)
     table.remove_command()
 
@@ -65,6 +62,7 @@ def rm(tag: str = "", grep: str = ""):
 @click.option('-t', '--tag', help="Search by tag")
 @click.option('-g', '--grep', help="Search by grep")
 def ls(tag: str = "", grep: str = ""):
+    init_config()    
     table = CommandTable(tag=tag, grep=grep)
     table.show_table()
 
@@ -72,6 +70,7 @@ def ls(tag: str = "", grep: str = ""):
 @click.option('-t', '--tag', help="Search by tag")
 @click.option('-g', '--grep', help="Search by grep")
 def run(tag: str = "", grep: str = ""):
+    init_config()
     table = CommandTable(tag=tag, grep=grep)
     table.run_command()
 
@@ -79,11 +78,13 @@ def run(tag: str = "", grep: str = ""):
 @click.option('-t', '--tag', help="Search by tag")
 @click.option('-g', '--grep', help="Search by grep")
 def cp(tag: str = "", grep: str = ""):
+    init_config()
     table = CommandTable(tag=tag, grep=grep)
     table.copy_command()
 
 @cli.command(help="Show tags 🏷️")
 @click.option('-g', '--grep', help="Search by grep")
 def tags(grep: str = ""):
+    init_config()
     table = CommandTable(grep=grep)
     table.show_tags()
